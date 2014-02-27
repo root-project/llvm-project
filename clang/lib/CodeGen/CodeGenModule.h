84e6a5447e7ab1a4ae68249b00957eb6aa67f745
Vassil Vassilev <vvasilev@cern.ch>
2014-02-27 15:50:37 +0100

    Add (debug) printout of ModuleBuilder data structures

--- clang/lib/CodeGen/CodeGenModule.h
+++ clang/lib/CodeGen/CodeGenModule.h
@@ -60,6 +60,7 @@ class FileSystem;
 namespace clang {
 class ASTContext;
 class AtomicType;
+class CodeGeneratorImpl; // hack needed by cling
 class FunctionDecl;
 class IdentifierInfo;
 class ObjCImplementationDecl;
@@ -2072,6 +2073,7 @@ private:
 
   llvm::Metadata *CreateMetadataIdentifierImpl(QualType T, MetadataTypeMap &Map,
                                                StringRef Suffix);
+  friend class clang::CodeGeneratorImpl; // hack needed by cling
 };
 
 }  // end namespace CodeGen
