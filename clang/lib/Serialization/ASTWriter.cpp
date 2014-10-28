2f837ff96f94e0a04e789aadfb30687c16feef6f
Axel Naumann <Axel.Naumann@cern.ch>
2014-10-28 18:20:18 +0100

    Store token literal data in PCH. Avoids disk read on PreProc expansion (ROOT-6794).

--- clang/lib/Serialization/ASTWriter.cpp
+++ clang/lib/Serialization/ASTWriter.cpp
@@ -5227,6 +5227,9 @@ void ASTWriter::AddToken(const Token &Tok, RecordDataImpl &Record) {
     // is needed.
     AddIdentifierRef(Tok.getIdentifierInfo(), Record);
   }
+
+  if (Tok.isLiteral())
+    AddString(StringRef(Tok.getLiteralData(), Tok.getLength()), Record);
 }
 
 void ASTWriter::AddString(StringRef Str, RecordDataImpl &Record) {
