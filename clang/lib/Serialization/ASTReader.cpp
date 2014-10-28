d85c950fee3c8853383cf5171e7b797c0b44fa9c
Axel Naumann <Axel.Naumann@cern.ch>
2014-10-28 18:20:18 +0100

    Store token literal data in PCH. Avoids disk read on PreProc expansion (ROOT-6794).

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -1700,6 +1700,14 @@ Token ASTReader::ReadToken(ModuleFile &F, const RecordDataImpl &Record,
     if (IdentifierInfo *II = getLocalIdentifier(F, Record[Idx++]))
       Tok.setIdentifierInfo(II);
   }
+
+  if (Tok.isLiteral()) {
+    const RecordData& RD = reinterpret_cast<const RecordData&>(Record);
+    std::string* Lit = new std::string(ReadString(RD, Idx));
+    TokenLiteralDataLoaded.push_back(Lit);
+    Tok.setLiteralData(Lit->c_str());
+  }
+
   return Tok;
 }
 
@@ -9982,6 +9990,9 @@ ASTReader::ASTReader(Preprocessor &PP, InMemoryModuleCache &ModuleCache,
 ASTReader::~ASTReader() {
   if (OwnsDeserializationListener)
     delete DeserializationListener;
+  for (auto PStr: TokenLiteralDataLoaded) {
+     delete PStr;
+  }
 }
 
 IdentifierResolver &ASTReader::getIdResolver() {
