90666b973cf4d24666a207fa6f07483f9f5f16c6
Vassil Vassilev <v.g.vassilev@gmail.com>
2019-02-21 09:29:38 +0200

    [cxxmodules] Don't complain when modulemap for implicit modules has changed
    This patch (also) aims to make runtime module installable.
    
    This part of code in Clang is comparing the location of "modulemap which
    is currently loaded and gives a definition of current module (say, stl)
    and "the location of the modulemap where the current implicit module (like stl) was built".
    
    This was problematic for CMSSW, as they should install modulemaps
    and prebuilt pcms to other directory. stl and libc pcms should be
    prebuilt, installed and used from installed directory, so this check is
    redundant for that usecase.

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -4210,9 +4210,13 @@ ASTReader::ReadModuleMapFileBlock(RecordData &Record, ModuleFile &F,
 
     assert(M && M->Name == F.ModuleName && "found module with different name");
 
+    // Check any additional module map files (e.g. module.private.modulemap)
+    // that are not in the pcm.
+    bool DisableValidation = PP.getPreprocessorOpts().DisablePCHOrModuleValidation !=
+                                  DisableValidationForModuleKind::None;
     // Check the primary module map file.
     auto StoredModMap = FileMgr.getFile(F.ModuleMapPath);
-    if (!StoredModMap || *StoredModMap != ModMap) {
+    if (!DisableValidation && (!StoredModMap || *StoredModMap != ModMap)) {
       assert(ModMap && "found module is missing module map file");
       assert((ImportedBy || F.Kind == MK_ImplicitModule) &&
              "top-level import should be verified");
@@ -4237,8 +4241,7 @@ ASTReader::ReadModuleMapFileBlock(RecordData &Record, ModuleFile &F,
       AdditionalStoredMaps.insert(*SF);
     }
 
-    // Check any additional module map files (e.g. module.private.modulemap)
-    // that are not in the pcm.
+    if (!DisableValidation) {
     if (auto *AdditionalModuleMaps = Map.getAdditionalModuleMapFiles(M)) {
       for (FileEntryRef ModMap : *AdditionalModuleMaps) {
         // Remove files that match
@@ -4260,6 +4263,7 @@ ASTReader::ReadModuleMapFileBlock(RecordData &Record, ModuleFile &F,
           << F.ModuleName << /*not new*/1 << ModMap.getName();
       return OutOfDate;
     }
+  }
   }
 
   if (Listener)
