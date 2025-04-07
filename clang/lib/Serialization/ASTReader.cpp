c93a54173f6a2cb864b25013c2a778c45a5ba69c
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2025-04-07 13:59:41 +0200

    PR133057: [Serialization] Fix lazy template loading
     * Hash inner template arguments: The code is applied from
       ODRHash::AddDecl with the reasoning given in the comment,
       to reduce collisions. This was particularly visible with
       STL types templated on std::pair where its template arguments
       were not taken into account.
     * Complete only needed partial specializations: It is unclear
       (to me) why this needs to be done "for safety", but this change
       significantly improves the effectiveness of lazy loading.
     * Load only needed partial specializations: Similar as the last
       commit, it is unclear why we need to load all specializations,
       including non-partial ones, when we have a TPL.
     * Remove bail-out logic in TemplateArgumentHasher: While it is
       correct to assign a single fixed hash to all template arguments,
       it can reduce the effectiveness of lazy loading and is not actually
       needed: we are allowed to ignore parts that cannot be handled
       because they will be analogously ignored by all hashings.

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -7707,14 +7707,8 @@ void ASTReader::CompleteRedeclChain(const Decl *D) {
     }
   }
 
-  if (Template) {
-    // For partitial specialization, load all the specializations for safety.
-    if (isa<ClassTemplatePartialSpecializationDecl,
-            VarTemplatePartialSpecializationDecl>(D))
-      Template->loadLazySpecializationsImpl();
-    else
-      Template->loadLazySpecializationsImpl(Args);
-  }
+  if (Template)
+    Template->loadLazySpecializationsImpl(Args);
 }
 
 CXXCtorInitializer **
